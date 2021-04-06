de94cac8782226d89eb9fd24f8753349ddaed11e
IsaacAds <isaac.ads@hotmail.com>
2021-04-06 12:58:08 -0300

    Listagem de Registro de Hora Extra

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.shortcuts import redirect
 
-# Create your models here.
+
 class Empresa(models.Model):
     nome = models.CharField(max_length=100, help_text='Nome da Empresa')
 
     def __str__(self):
         return self.nome
+
+    def get_absolute_url(self):
+        return redirect('/')
